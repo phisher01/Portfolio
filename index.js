a66f74d3fbce51a410bc51e715877088ec357169
Gagan Singh <gsgagan2003@gmail.com>
2024-09-01 22:59:22 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+const port=3035;
+app.use(express.static(path.join(__dirname,"/views")));
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+app.listen(port,()=>{   // server starts
+    console.log(`listening on port ${port}`);
+    
+});
+
+app.get("/home",(req,res)=>{
+   
+    res.render("home.ejs");
+});
+app.get("/projects",(req,res)=>{
+   
+    res.render("projects.ejs");
+});
+app.get("/education",(req,res)=>{
+   
+    res.render("education.ejs");
+});
+app.get("/otherprofiles",(req,res)=>{
+   
+    res.render("profiles.ejs");
+});
