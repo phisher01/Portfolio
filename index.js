9ca5de10d3c125ff2b4dac09a6e2f1afc8a323c4
Gagan Singh <gsgagan2003@gmail.com>
2024-09-02 01:09:25 +0530

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app=express();
 const path=require("path");
-const port=9091;
+const port=8080;
 app.use(express.static(path.join(__dirname,"/views")));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
